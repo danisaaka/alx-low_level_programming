453108fb975b5f035a6a0ab1561ff798c503b1d7
Dan Isaaka <isaakadan38@hotmail.com>
2022-03-16 23:24:45 +0300

    Task 1 update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,35 +5,14 @@
  * print_alphabet -function
  * Return: Always 0 (Success)
  */
-void trimTrailing(char * x);
 void print_alphabet(void)
 {
 	char x;
-	
+
 	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
 	putchar('\n');
-}
-
-void trimTrailing(char * x)
-{
-	int index, i;
-
-	index = -1;
-
-	i = 0;
-
-	while(x[i] != '\0')
-	{
-		if(x[i] != ' ' && x[i] != '\t' && x[i] != '\n')
-		{
-			index = i;
-		}
-		
-		i++;
-	}
-	x[index + 1] = '\0';
 }
 
