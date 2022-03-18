84abaac8f950742cade3d9b36079a536a39bd659
Dan Isaaka <isaakadan38@hotmail.com>
2022-03-19 02:06:19 +0300

    0x03 - Debugging directory and task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	
+	printf(positive_or_negative(i));
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+0x03-Debugging
+Learning how to find errors in your code
+-Rubber Duck programming
