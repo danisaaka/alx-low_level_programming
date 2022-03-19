28995be53f1927f885cc85b6095857c60ba53aa1
Dan Isaaka <isaakadan38@hotmail.com>
2022-03-19 03:03:07 +0300

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -12,8 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
-	
+	i = 0;	
 	printf(positive_or_negative(i));
 
 	return (0);
