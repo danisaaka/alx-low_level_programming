423c5fb293c7c0564dece12b355e13b519b4f50c
Dan Isaaka <isaakadan38@hotmail.com>
2022-03-21 22:53:41 +0300

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character starting with the first
+ * character, followed by a new line.
+ * @str: the string to manipulate.
+ *
+ * Return: nothing.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (!(i % 2))
+			putchar(str[i]);
+	putchar(10);
+}
