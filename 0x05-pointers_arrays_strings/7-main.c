e25623ff1ea04e3b05e4b27e46d95898b95459d3
Dan Isaaka <isaakadan38@hotmail.com>
2022-03-21 23:02:54 +0300

    Tsk 7

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
